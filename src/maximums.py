3ad2cddd35eece0448efdb20a066c0dc39af930a
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-08-21 19:54:37 -0300

    Update maximums.py

--- src/maximums.py
+++ src/maximums.py
@@ -6,21 +6,12 @@ def max_of_two(x, y):
         biggest = y
         return biggest
 
-# Replace the "ANSWER HERE" for your answer
 def max_of_three(x, y, z):
-    max = x
-    if x > y and x>z:
-        return max
-    elif y>x and y>z:
-        return max
-    elif z>x and z>y:
-        return max
-    elif x==y:
-        return max
-    elif x==z:
-        return max
+    if x >= y and x >= z:
+        return x
+    elif y >= x and y >= z:
+        return y
     else:
-        max=y
-        return max
+        return z
 
 
