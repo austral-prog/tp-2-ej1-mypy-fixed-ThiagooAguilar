31067f77efa45736524a038324871c0d7b0536ff
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-08-21 19:48:58 -0300

    Update leap_year.py

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,3 +1,9 @@
 # Replace the "ANSWER HERE" for your answer
-def is_leap_year():
-    return "ANSWER HERE" # Remove this line and implement
+def leap_year():
+    my_year=int(input("Ingrese un año: "))
+    if ((my_year%4 == 0) and (my_year%100 !=0)):
+        print(f"El año {my_year} es bisiesto")
+    elif ((my_year%100==0) and (my_year%400==0)):
+        print(f"El año {my_year} es bisiesto")
+    else:
+        print(f"El año {my_year} no es bisiesto")
