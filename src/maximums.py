ad479b1da100cd2948987a29adb9640dcabaab54
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-08-29 10:42:08 -0300

    Update maximums.py

--- src/maximums.py
+++ src/maximums.py
@@ -1,12 +1,11 @@
-def max_of_two(x, y):
+def max_of_two(x: int, y: int) -> int:
     biggest = x
     if x >= y:
         return biggest
     else:
         biggest = y
         return biggest
-
-def max_of_three(x, y, z):
+def max_of_three(x: int, y: int, z: int)-> int:
     if x >= y and x >= z:
         return x
     elif y >= x and y >= z:
