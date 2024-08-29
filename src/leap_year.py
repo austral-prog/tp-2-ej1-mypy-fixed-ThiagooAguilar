8d40e7eb2155b1f04f5face9fd957e3aae770fba
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-08-28 21:32:25 -0300

    Update leap_year.py

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,5 +1,5 @@
 # Replace the "ANSWER HERE" for your answer
-def is_leap_year(year):
+def is_leap_year(year : int):
     if (year % 4 == 0):
         if (year % 100 == 0):
             if (year % 400 == 0):
@@ -12,8 +12,7 @@ def is_leap_year(year):
         return False
 
 def main():
-    year = int(input("Ingrese un año: "))
-    is_leap = is_leap_year(year)
-    print(f"El año {year} es {'bisiesto' if is_leap else 'no bisiesto'}")
+    año: int= int(input("Ingrese un año: "))
+    is_leap = is_leap_year(año)
+    print(f"El año {año} es {'bisiesto' if is_leap else 'no bisiesto'}")
     return is_leap
-
